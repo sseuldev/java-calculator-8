d4588b24034016b108659ddc12537ea0a4bbd29b
sseuldev <lhs010107@gmail.com>
2025-10-19 02:52:41 +0900

    feat : Application 의존성 주입 Controller에 model의 메인인 Calculator 객체와 view인 Input, Output 객체를 주입하는 코드 구현하였습니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.view.Input;
+import calculator.view.Output;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Input input = new Input();
+        Output output = new Output();
+        Calculator calculator = new Calculator();
+        CalculatorController calculatorController = new CalculatorController(input, output, calculator);
+        calculatorController.run();
     }
 }

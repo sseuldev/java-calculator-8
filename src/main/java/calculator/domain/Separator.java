8ce79332ff8533f79bd1e5c1bd439b945aafc51e
sseuldev <lhs010107@gmail.com>
2025-10-19 02:45:00 +0900

    feat(Separator) : 구분자 기준으로 문자 분리 로직 구현 기본 구분자 및 커스텀 구분자로 문자 분리하는 로직 구현하였습니다. 이와 함께 '구분자 관련 에러' 전체를 해당 클래스가 전담하도록 구현하였습니다.

---
+++ src/main/java/calculator/domain/Separator.java
@@ -0,0 +1,74 @@
+package calculator.domain;
+
+import java.util.regex.Pattern;
+
+public class Separator {
+
+    private static final String INVALID_CUSTOM_SEPARATOR_LENGTH = "커스텀 구분자는 하나의 문자여야 합니다.";
+    private static final String INVALID_CUSTOM_SEPARATOR_TYPE = "커스텀 구분자는 숫자 타입일 수 없습니다.";
+    private static final String ALREADY_HAVE_BASIC_SEPARATOR = "커스텀 구분자는 기본 구분자가 아니어야 합니다.";
+    private static final String BASIC_SEPARATOR = "[,:]";
+
+    private String customSeparator = null;
+
+    public Separator(String input) {
+        setCustomSeparator(input);
+    }
+
+    public String[] splitBySeparator(String input) {
+        if (customSeparator == null) {
+            return input.split(BASIC_SEPARATOR);
+        }
+
+        String target;
+
+        if (customSeparator.isEmpty()) {
+            target = input.substring(4);
+        }
+        else {
+            target = input.substring(5);
+        }
+
+        return target.split(BASIC_SEPARATOR + "|" + customSeparator);
+    }
+
+    private void setCustomSeparator(String input) {
+        if (hasCustomSetting(input)) {
+            String separator = getCustomSeparator(input);
+            validate(separator);
+            this.customSeparator = Pattern.quote(separator);
+        }
+    }
+
+    private boolean hasCustomSetting(String input) {
+        return input.startsWith("//") && input.indexOf("\\n") >= 2;
+    }
+
+    private String getCustomSeparator(String input) {
+        return input.substring(2, input.indexOf("\\n"));
+    }
+
+    private void validate(String separator) {
+        validateNumOfSeparator(separator);
+        validateLetter(separator);
+        validateNotBasicSeparator(separator);
+    }
+
+    private void validateNumOfSeparator(String separator) {
+        if (separator.length() > 1) {
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_LENGTH);
+        }
+    }
+
+    private void validateLetter(String separator) {
+        if (separator.matches("\\d")) {
+            throw new IllegalArgumentException(INVALID_CUSTOM_SEPARATOR_TYPE);
+        }
+    }
+
+    private void validateNotBasicSeparator(String separator) {
+        if (!separator.isEmpty() && ",:".contains(separator)) {
+            throw new IllegalArgumentException(ALREADY_HAVE_BASIC_SEPARATOR);
+        }
+    }
+}
